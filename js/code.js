8d020f9618c5df216c178e5113fc0449e0eb9814
Lubin Mendez <lubin.mendez.developer@gmail.com>
2025-05-03 22:43:20 -0700

    DOM Selections: digits and buttons for hours, minutes, and seconds

--- js/code.js
+++ js/code.js
@@ -1,4 +1,14 @@
 // alert("conected");
+//SELECTIONS:
+let selBtnPlay= document.querySelector(".btnPlay");
+let selBtnPause= document.querySelector(".btnPause");
+let selBtnReset= document.querySelector(".btnReset");
+let selHrs= document.querySelector(".hrs");
+let selMins= document.querySelector(".mins");
+let selSecs= document.querySelector(".secs");
+
+
+
 
 
 
