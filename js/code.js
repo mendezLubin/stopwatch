fed03be2d6a6abf649df8757870501151becb5e9
Lubin Mendez <lubin.mendez.developer@gmail.com>
2025-05-04 21:52:45 -0700

    Adding global variables

--- js/code.js
+++ js/code.js
@@ -1,5 +1,5 @@
 // alert("conected");
-//SELECTIONS:
+// SELECTIONS:
 let selBtnPlay= document.querySelector(".btnPlay");
 let selBtnPause= document.querySelector(".btnPause");
 let selBtnReset= document.querySelector(".btnReset");
@@ -7,7 +7,8 @@ let selHrs= document.querySelector(".hrs");
 let selMins= document.querySelector(".mins");
 let selSecs= document.querySelector(".secs");
 
-
+let secondsCounter= 0; // Global variable to count seconds
+let intervalId= null; // Global variable and intentionally left empty
 
 
 
